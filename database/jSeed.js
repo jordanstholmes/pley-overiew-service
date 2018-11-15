c1f2ed7b4165d2c6e2528c21055a5200fd5d8ca6
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-14 23:27:58 -0800

    Create seed generation script

--- database/jSeed.js
+++ database/jSeed.js
@@ -2,106 +2,88 @@ const faker = require('faker');
 const fs = require('fs');
 const path = require('path');
 // const db = require('./index.js');
+let restaurantId = 1;
+let imageId = 1;
+const BATCH_SIZE = 1000;
+const FOODCATEGORIES = ['drinks', 'food'];
+const AVERAGE_IMAGES_PER_RESTAURANT = 10;
 
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+function padIndex(idx) {
+  const zeroCount = 4 - idx.toString().length; // 4 because indexes go up to 1000 (4 digits)
+  return Array(zeroCount).fill('0').join('') + idx.toString();
+}
 
-const FOODCATEGORIES = ['drinks', 'food']; // 3/4th chance of food
-
-// Split this up into modular functions
-// Write to database/seedFiles
-
-const seed = (numberOfInstances = 1) => {
+const seedRestaurants = (numberOfInstances = BATCH_SIZE) => {
+  // schema: (N)id, name, address, phone, website, googleMap, (N)cost
+  let csvLines = '';
   let name;
   let address;
   let cost;
   let phone;
   let website;
   let googleMap;
-  const data = {
-    restaurants: [],
-    images: [],
-  };
-
   // seeding the restaurants table
   for (let i = 0; i < numberOfInstances; i += 1) {
+    // name
     name = faker.company.companyName();
     address = [faker.address.streetAddress(), faker.address.city(), faker.address.state(), faker.address.zipCode()].join(', ');
     cost = getRandomInt(5) + 1;
     phone = faker.phone.phoneNumberFormat();
     website = faker.internet.url();
     googleMap = `https://s3-us-west-1.amazonaws.com/yump-sf-overview/maps/${getRandomInt(5) + 1}.png`;
-    data.restaurants.push({
-      name,
-      address,
-      cost,
-      phone,
-      website,
-      googleMap,
-    });
+    csvLines += `"${restaurantId}","${name}","${address}","${phone}","${website}","${googleMap}","${cost}"\n`;
+    restaurantId += 1;
   }
-  return JSON.stringify(data.restaurants);
-
-  // let user;
-  // let description;
-  // let date;
-  // let category;
-  // let restaurant;
-  // let image;
-  // // seeding the image table
-  // for (let j = 0; j < 2000; j += 1) {
-  //   user = faker.name.findName(); // Rowan Nikolaus
-  //   description = faker.lorem.sentences();
-  //   date = faker.date.recent();
-  //   category = FOODCATEGORIES[getRandomInt(FOODCATEGORIES.length)];
-  //   restaurant = getRandomInt(numberOfInstances) + 1;
-
-  //   if (category === 'food') {
-  //     image = `https://s3-us-west-1.amazonaws.com/yump-sf-overview/${randomCategory}/${getRandomInt(18) + 1}.jpg`;
-  //   } else if (category === 'drinks') {
-  //     image = `https://s3-us-west-1.amazonaws.com/yump-sf-overview/${randomCategory}/${getRandomInt(7) + 1}.jpg`;
-  //   }
-  // }
+  return csvLines;
 };
 
-function createSeedWriter() {
-  const destination = path.resolve(__dirname, `./seedFiles/testSeed${1}.json`);
+const seedImages = (numberOfInstances = BATCH_SIZE) => {
+  let csvLines = '';
+  let user;
+  let description;
+  let posted;
+  let category;
+  let restaurant;
+  let image;
+  // seeding the image table
+  for (let j = 0; j < numberOfInstances; j += 1) {
+    user = faker.name.findName(); // Rowan Nikolaus
+    description = faker.lorem.sentences();
+    posted = faker.date.recent();
+    category = FOODCATEGORIES[getRandomInt(FOODCATEGORIES.length)];
+    restaurant = getRandomInt(BATCH_SIZE) + 1;
+    const paddedImgIdx = padIndex(getRandomInt(900 + 1)); // 1 of 900 images in s3
+    image = `https://s3-us-west-1.amazonaws.com/sdc-overview-images/images/${paddedImgIdx}.jpeg`;
+    csvLines += `"${imageId}","${user}","${image}","${description}","${posted}","${category}","${restaurant}"\n`;
+    imageId += 1;
+  }
+  return csvLines;
+};
+
+function createSeedWriter(fileName, total, seedGenerator, cb) {
+  let totalSeedEntries = total / BATCH_SIZE;
+  const destination = path.resolve(__dirname, `./seedFiles/${fileName}`);
   const stream = fs.createWriteStream(destination);
-  let numberOfSeed = 1000000;
 
   const writer = () => {
     let result = true;
-    while (result && numberOfSeed) {
-      result = stream.write(seed(10));
-      numberOfSeed -= 1;
+    while (result && totalSeedEntries > 0) {
+      result = stream.write(seedGenerator());
+      totalSeedEntries -= 1;
     }
-    if (numberOfSeed) {
+    if (totalSeedEntries > 0) {
+      console.log((totalSeedEntries * BATCH_SIZE), 'REMAINING');
       stream.once('drain', writer);
+    } else if (totalSeedEntries <= 0 && cb) {
+      console.log('Switching to images now...');
+      cb();
     }
   };
   return writer;
 }
 
-const writer = createSeedWriter();
-writer();
-// function writeSeedCSV() {
-//   let stream;
-//   if (!writeStream) {
-//     const destination = path.resolve(__dirname, `./seedFiles/testSeed${1}.json`);
-//     stream = fs.createWriteStream(destination);
-//   } else {
-//     stream = writeStream;
-//   }
-//   stream.write(seed(1));
-//   stream.on('finish', () => {
-//     console.log('wrote data to file');
-//     if (iterations) {
-//       writeSeedCSV(iterations - 1, stream);
-//     } else {
-//       writeStream.end();
-//       console.log('finished all iterations!');
-//     }
-//   });
-// }
-// seed(100);
-
-// setTimeout((() => process.exit()), 2000); // What does this do?
+const imageWriter = createSeedWriter('images.csv', 1000000, seedImages);
+const restaurantWriter = createSeedWriter('restaurants.csv', 100000, seedRestaurants, imageWriter);
+restaurantWriter();
+// imageWriter();
