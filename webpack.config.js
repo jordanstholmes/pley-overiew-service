b07f356ca0a2a0094cec5465e623beb925d2ba16
Jordan Holmes <jordanstholmes@gmail.com>
2018-11-24 21:29:39 -0800

    Clean up linting issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const CompressionPlugin = require('compression-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './client/index.jsx',
   output: {
-    path: __dirname + '/public/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   plugins: [
     new CompressionPlugin(),
   ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
-        loaders: "babel-loader",
+        loaders: 'babel-loader',
         test: /\.jsx?$/,
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        }
-      }
-    ]
-  }
-}
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
